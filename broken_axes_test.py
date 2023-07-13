8d2792fcb3f0f9b3d53a6a28155109ac19296fc7
Qwert512 <Robinmu1024@gmail.com>
2023-07-13 16:50:27 +0200

    some comments

--- broken_axes_test.py
+++ broken_axes_test.py
@@ -45,6 +45,10 @@ test_json_data = {
 }
 
 def plot(json_data:dict,x_width:float,y_height:float,x_axis_mins:int):
+    if y_height < 1:
+        y_height = 1
+    if x_width < 1:
+        x_width = 1
     #define some variables and fetch some basic data
     jumps_1 = ()
     jumps_2 = ()
@@ -58,16 +62,19 @@ def plot(json_data:dict,x_width:float,y_height:float,x_axis_mins:int):
     
     if first_key_1 <= 0:
         first_key_1 = 0
+        #check if it neccesary to remove values based on the provided time window
     else:
         for i in reversed(keys_1):
             i = int(i)
-
+        #iterate through the time values in minutes
             if i < first_key_1:
                 json_data["raw_minutes"]["rohr_1"].pop(str(i))
                 keys_1.remove(str(i))
+        #then pop all the values that do not fit in the specified time window
 
     if first_key_2 <= 0:
         first_key_2 = 0
+        #see for the "rohr_1" code block
     else:
         for i in reversed(keys_2):
             i = int(i)
@@ -81,7 +88,7 @@ def plot(json_data:dict,x_width:float,y_height:float,x_axis_mins:int):
     for i in range(len(keys_1) - 1):
         if int(keys_1[i+1]) > int(keys_1[i]) + 1:
             old_tuple = jumps_1
-            new_tuple = old_tuple + ((previous_num, int(keys_1[i])),)  # Create a new tuple with two elements
+            new_tuple = old_tuple + ((previous_num, int(keys_1[i])),)
             previous_num = int(keys_1[i+1])
             jumps_1 = new_tuple
     old_tuple = jumps_1
@@ -92,13 +99,14 @@ def plot(json_data:dict,x_width:float,y_height:float,x_axis_mins:int):
         modified_tuple = [*jumps_1[-1][:-1], jumps_1[-1][-1] + 1]
         jumps_1 = tuple([list(t) if t != jumps_1[-1] else modified_tuple for t in jumps_1])
 
-
+    #this is pretty complicated, just ignore it. It does some black magic and then the data is 
+    #sorted, crisp, cleaned and stuffed into the right output format
 
     previous_num = first_key_2
     for i in range(len(keys_2) - 1):
         if int(keys_2[i+1]) > int(keys_2[i]) + 1:
             old_tuple = jumps_2
-            new_tuple = old_tuple + ((previous_num, int(keys_2[i])),)  # Create a new tuple with two elements
+            new_tuple = old_tuple + ((previous_num, int(keys_2[i])),)
             previous_num = int(keys_2[i+1])
             jumps_2 = new_tuple
     old_tuple = jumps_2
@@ -111,7 +119,7 @@ def plot(json_data:dict,x_width:float,y_height:float,x_axis_mins:int):
 
     # Create a figure and axes
 
-    fig = plt.figure(figsize=(x_width, y_height))
+    plt.figure(figsize=(x_width, y_height))
     ax = brokenaxes(xlims=jumps_1, hspace=0.05)
 
     # Plot data on the broken axes
@@ -120,4 +128,4 @@ def plot(json_data:dict,x_width:float,y_height:float,x_axis_mins:int):
 
     # Show the plot
     plt.show()
-plot(test_json_data,8,4,900)
+plot(test_json_data,1,1,900)
